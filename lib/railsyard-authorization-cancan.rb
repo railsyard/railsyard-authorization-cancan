2a50a14028efd9a0971b33e7ad6a04421fb37494
Stefano Verna <stefano.verna@welaika.com>
2012-03-27 16:10:27 +0200

    Initial commit

---
+++ lib/railsyard-authorization-cancan.rb
@@ -0,0 +1,9 @@
+require "railsyard-authorization-cancan/version"
+
+module Railsyard
+  module Authorization
+    module Cancan
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/railsyard-authorization-cancan/version.rb
@@ -0,0 +1,7 @@
+module Railsyard
+  module Authorization
+    module Cancan
+      VERSION = "0.0.1"
+    end
+  end
+end
